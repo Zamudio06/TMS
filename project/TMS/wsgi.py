909202b50ddf7646333f875431ade2524d7024ac
David Zamudio <85513779+Zamudio06@users.noreply.github.com>
2024-10-17 22:59:25 -0600

    first commit

--- project/TMS/wsgi.py
+++ project/TMS/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TMS.production-settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.TMS.production-settings')
 
 application = get_wsgi_application()
