c0c0f0bd9f5f4eb88e5c306d0e926773e36d0a41
David Zamudio <85513779+Zamudio06@users.noreply.github.com>
2024-10-18 01:29:09 -0600

    first commit

--- task_management/models.py
+++ task_management/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
-    email = models.EmailField(null=False)
+    email = models.EmailField(null=False, help_text="Correo utilizado para enviar notificaciones")
     description = models.TextField()
     
     def __str__(self):
